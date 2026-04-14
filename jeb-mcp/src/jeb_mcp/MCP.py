91e8892879a27613d05c36d3466095670ac13136
wrlu <wrlus@outlook.com>
2026-04-14 11:29:18 +0800

    Fix syntax.

--- jeb-mcp/src/jeb_mcp/MCP.py
+++ jeb-mcp/src/jeb_mcp/MCP.py
@@ -1046,7 +1046,7 @@ def get_strings(filepath, regex_pattern="", limit=100):
                         if limit > 0 and len(results) >= limit:
                             break
             except Exception as e:
-                print("Failed to parse string due to: " + e)
+                print("Failed to parse string on " + str(s) + " due to: " + str(e))
                 continue
                     
     return results
